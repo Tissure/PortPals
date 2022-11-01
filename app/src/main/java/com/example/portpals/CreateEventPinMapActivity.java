7cb7500b8c78b4e5946ec5f3dac7050a2b6dd1f3
JungMin Oh <joh67@my.bcit.ca>
2022-11-01 11:43:49 -0700

    Added activities for users to create an event

---
+++ app/src/main/java/com/example/portpals/CreateEventPinMapActivity.java
@@ -0,0 +1,2 @@
+package com.example.portpals;public class CreateEventPinMapActivity {
+}
---
+++ app/src/main/java/com/example/portpals/CreateEventTagActivity.java
@@ -0,0 +1,14 @@
+package com.example.portpals;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateEventTagActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_event_tag);
+    }
+}
