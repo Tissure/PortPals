ab8a5ea34673e2131ffbca84e3d98a6a3f091157
Clayton Hunter <claytonhunter10@gmail.com>
2022-11-21 11:11:09 -0800

    added profile picture upload for the sign in page

--- app/src/main/java/com/example/portpals/SignUpActivity.java
+++ app/src/main/java/com/example/portpals/SignUpActivity.java
@@ -1,18 +1,35 @@
 package com.example.portpals;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.portpals.models.User;
 import com.example.portpals.util.Validator;
+import com.google.android.gms.cast.framework.media.ImagePicker;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
+import java.util.UUID;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -23,6 +40,12 @@ public class SignUpActivity extends AppCompatActivity {
     private TextView passwordTextView;
     private TextView passwordConfirmTextView;
     private TextView displayNameTextView;
+    private ImageView profileImageView;
+    private Button uploadProfilePictureButton;
+
+    private Uri filePath;
+    private FirebaseStorage storage;
+    private StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,12 +69,92 @@ public class SignUpActivity extends AppCompatActivity {
             }
         });
 
+        // add functionality for uploading an image
+        profileImageView = findViewById(R.id.profileImageView);
+        uploadProfilePictureButton = findViewById(R.id.uploadProfilePictureButton);
+        uploadProfilePictureButton.setOnClickListener(view -> {
+            selectImage();
+        });
+
         // add go back to sign in button functionality
         Button toSignIn = findViewById(R.id.signInButton);
         toSignIn.setOnClickListener(view -> {
             Intent intent = new Intent(this, SignInActivity.class);
             startActivity(intent);
         });
+    }
+
+    private void selectImage() {
+        // Defining Implicit Intent to mobile gallery
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivity(
+                Intent.createChooser(
+                        intent,
+                        "Select Image from here..."));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        final int PICK_IMAGE_REQUEST = 22;
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            // Get the Uri of data
+            filePath = data.getData();
+            try {
+                // Setting image on image view using Bitmap
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                profileImageView.setImageBitmap(bitmap);
+            }
+
+            catch (IOException e) {
+                // Log the exception
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // UploadImage method
+    private void uploadImage() {
+        if (filePath != null) {
+            // Code for showing progressDialog while uploading
+            ProgressDialog progressDialog
+                    = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+
+            // Defining the child of storageReference
+            StorageReference ref = storageReference.child("images/" + FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+            // adding listeners on upload
+            // or failure of image
+            // Progress Listener for loading percentage on the dialog box
+            ref.putFile(filePath)
+                    .addOnSuccessListener(
+                            taskSnapshot -> {
+                                // Image uploaded successfully
+                                // Dismiss dialog
+                                progressDialog.dismiss();
+                                Toast.makeText(getApplicationContext(), "Image Uploaded!!", Toast.LENGTH_SHORT).show();
+                            })
+
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            // Error, Image not uploaded
+                            progressDialog.dismiss();
+                            Toast.makeText(getApplicationContext(), "Failed " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(taskSnapshot -> {
+                                double progress = (100.0
+                                        * taskSnapshot.getBytesTransferred()
+                                        / taskSnapshot.getTotalByteCount());
+                                progressDialog.setMessage("Uploaded " + (int)progress + "%");
+                    });
+        }
     }
 
     private boolean checkFields() {
@@ -80,6 +183,10 @@ public class SignUpActivity extends AppCompatActivity {
                     // attempt to put the new user in the database
                     User newUser = new User(email, displayName, firstName, lastName);
                     Task putUserInDB = MainActivity.databaseReference.child("Users").child(key).setValue(newUser);
+
+                    // upload the user profile to the firebase cloud
+                    uploadImage();
+
                     if (!putUserInDB.isSuccessful()) {
                         String errMsg = putUserInDB.getException().getMessage();
                         Toast.makeText(this, errMsg,  Toast.LENGTH_LONG).show();
