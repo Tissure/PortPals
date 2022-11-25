7cc3d40dbe6e9470813a4e6bea460a6fba03192a
Clayton Hunter <claytonhunter10@gmail.com>
2022-11-24 16:34:32 -0800

    started working on the profile page

--- app/src/main/java/com/example/portpals/SignUpActivity.java
+++ app/src/main/java/com/example/portpals/SignUpActivity.java
@@ -1,8 +1,6 @@
 package com.example.portpals;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.loader.content.AsyncTaskLoader;
 
 import android.content.Intent;
 import android.net.Uri;
@@ -16,15 +14,12 @@ import android.widget.Toast;
 
 import com.example.portpals.models.User;
 import com.example.portpals.util.Validator;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-
-import java.util.UUID;
 
 public class SignUpActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/portpals/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/portpals/fragments/ProfileFragment.java
@@ -1,6 +1,5 @@
 package com.example.portpals.fragments;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,10 +7,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
-import com.example.portpals.EventHistoryActivity;
 import com.example.portpals.R;
-import com.example.portpals.SignInActivity;
+import com.example.portpals.models.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 
 public class ProfileFragment extends Fragment implements View.OnClickListener {
 
@@ -20,26 +27,55 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_profile, container,false);
         //Create a path to listen for a click for each button
-        rootView.findViewById(R.id.btnToSignUp).setOnClickListener(this);
-        rootView.findViewById(R.id.btnEventHistory).setOnClickListener(this);
+        rootView.findViewById(R.id.profile_page_picture).setOnClickListener(this);
+        rootView.findViewById(R.id.event_history_button).setOnClickListener(this);
+        rootView.findViewById(R.id.logout_button).setOnClickListener(this);
+        rootView.findViewById(R.id.edit_profile_button).setOnClickListener(this);
         // Inflate the layout for this fragment
         return rootView;
-
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
-            case R.id.btnToSignUp:
-                Intent intent = new Intent(getActivity(), SignInActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.btnEventHistory:
-                Intent intent2 = new Intent(getActivity(), EventHistoryActivity.class);
-                startActivity(intent2);
+            case R.id.profile_page_picture:
+                setProfilePicture((ImageView) view);
                 break;
         }
+    }
 
+    private void setProfilePicture(ImageView profilePictureView) {
+        FirebaseUser loggedInUser = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+        databaseReference.child("Users").get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                for (DataSnapshot userSnapShot : task.getResult().getChildren()) {
+                    if (userSnapShot.getKey().equals(loggedInUser.getUid())) {
+                        User user = userSnapShot.getValue(User.class);
+                        String imageKey = user.getProfilePictureKey();
+
+                        // set the profile picture if it exists
+                        StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+                        if (user != null) {
+                            StorageReference profilePictureRef = storageReference.child("images/" + imageKey);
+                            if (profilePictureRef != null) {
+                                profilePictureRef.getDownloadUrl().addOnCompleteListener(task2 -> {
+                                    if (task2.isSuccessful()) {
+                                        Picasso.get().load(task2.getResult()).into(profilePictureView);
+                                    } else {
+                                        System.out.println("Failed to get profile picture and put it on the event");
+                                    }
+                                });
+                            }
+                        }
+                        break;
+                    }
+                }
+            } else {
+                System.out.println("Failed to obtain profile information from Firestore");
+                System.out.println(task.getException().getMessage());
+            }
+        });
     }
 
 }
